12d20a133d5dd8c29800d26410a9483469679442
martinh2k3 <martin.horsky.2003@gmail.com>
2025-10-03 09:51:55 +0200

    fix: Schema adjusted to support missing values

--- schema.py
+++ schema.py
@@ -1,89 +1,91 @@
 from pydantic import BaseModel, Field
 
 class IgnoreExtraModel(BaseModel):
-    class Config:
-        extra = 'ignore'
+    def __str__(self):
+        return self.model_dump_json(indent=2)
+
+    model_config = {
+        "extra": "ignore"
+    }
 
 
 class User(IgnoreExtraModel):
     id: int
-    name: str = Field(default='')
-    screen_name: str = Field(default='')
-    location: str = Field(default='')
-    url: str = Field(default='')
-    description: str = Field(default='')
+    name: str | None = Field(default='')
+    screen_name: str | None = Field(default='')
+    location: str | None = Field(default='')
+    url: str | None = Field(default='')
+    description: str | None = Field(default='')
     protected: bool | None = Field(default=None)
     verified: bool | None = Field(default=None)
-    followers_count: int = Field(default=0)
-    friends_count: int = Field(default=0)
-    listed_count: int = Field(default=0)
-    favourites_count: int = Field(default=0)
-    statuses_count: int = Field(default=0)
+    followers_count: int | None = Field(default=0)
+    friends_count: int | None = Field(default=0)
+    listed_count: int | None = Field(default=0)
+    favourites_count: int | None = Field(default=0)
+    statuses_count: int | None = Field(default=0)
     created_at: str | None = Field(default=None) # UTC datetime
 
 
 class Place(IgnoreExtraModel):
-    id: str = Field(default='')
-    place_type: str = Field(default='')
-    full_name: str = Field(default='')
-    country_code: str = Field(default='')
-    country: str = Field(default='')
+    id: str
+    place_type: str | None = Field(default='')
+    full_name: str | None = Field(default='')
+    country_code: str | None = Field(default='')
+    country: str | None = Field(default='')
 
 
 class Hashtag(BaseModel):
-    text: str = Field(default='')
+    text: str | None = Field(default='')
 
 
 class UnwoundUrl(IgnoreExtraModel):
-    url: str = Field(default='')
+    url: str | None = Field(default='')
     status: int | None = Field(default=None)
-    title: str = Field(default='')
-    description: str = Field(default='')
+    title: str | None = Field(default='')
+    description: str | None = Field(default='')
 
 
 class Url(IgnoreExtraModel):
-    url: str = Field(default='')
-    expanded_url: str = Field(default='')
-    display_url: str = Field(default='')
+    url: str | None = Field(default='')
+    expanded_url: str | None = Field(default='')
+    display_url: str | None = Field(default='')
     unwound_url: UnwoundUrl | None = Field(default=None)
 
 
 class Media(IgnoreExtraModel):
     id: int
-    display_url: str = Field(default='')
-    expanded_url: str = Field(default='')
-    media_url: str = Field(default='')
-    media_url_https: str = Field(default='')
-    type: str = Field(default='')
+    display_url: str | None = Field(default='')
+    expanded_url: str | None = Field(default='')
+    media_url: str | None = Field(default='')
+    media_url_https: str | None = Field(default='')
+    type: str | None = Field(default='')
 
 
 class UserMention(IgnoreExtraModel):
     id: int # User ID
-    screen_name: str = Field(default='')
-    name: str = Field(default='')
+    screen_name: str | None = Field(default='')
+    name: str | None = Field(default='')
 
 class Entities(IgnoreExtraModel):
-    hashtags: list[Hashtag] = Field(default_factory=list)
-    user_mentions: list[UserMention] = Field(default_factory=list)
-    urls: list[Url] = Field(default_factory=list)
-    media: list[Media] | None = Field(default=None)
+    hashtags: list[Hashtag] | None = Field(default_factory=list)
+    user_mentions: list[UserMention] | None = Field(default_factory=list)
+    urls: list[Url] | None = Field(default_factory=list)
+    media: list[Media] | None = Field(default_factory=list)
 
 
 class Tweet(IgnoreExtraModel):
     id: int
-    text: str = Field(default='')
-    source: str = Field(default='')
+    text: str | None = Field(default='')
+    source: str | None = Field(default='')
     in_reply_to_status_id: int | None = Field(default=None)
     quoted_status_id: int | None = Field(default=None)
-    retweet_count: int = Field(default=0)
-    favorite_count: int = Field(default=0)
-    created_at: str = Field(default='') # UTC datetime
-    lang: str = Field(default='')
+    retweet_count: int | None = Field(default=0)
+    favorite_count: int | None = Field(default=0)
+    created_at: str | None = Field(default='') # UTC datetime
+    lang: str | None = Field(default='')
     possibly_sensitive: bool | None = Field(default=None)
     display_text_range: tuple[int, int] | None = Field(default=None)
-    retweeted_status: BaseModel = Field(default=None) # References another Tweet object
-    user: User = Field(default=None)
-    place: Place = Field(default=None)
-    entities: Entities = Field(default=None)
-
-
+    retweeted_status: BaseModel | None = Field(default=None) # References another Tweet object
+    user: User | None = Field(default=None)
+    place: Place | None = Field(default=None)
+    entities: Entities | None = Field(default=None)
