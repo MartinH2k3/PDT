53e42cbb1f960b4f121168b5e71c57ad0c8c79a9
martinh2k3 <martin.horsky.2003@gmail.com>
2025-10-03 11:16:40 +0200

    feat: added method to clean up schema and started working on loading the data

--- utils.py
+++ utils.py
@@ -37,6 +37,32 @@ def build_schema(schema_file_name: str = 'schema.sql', connection = None):
         if not connection_passed:
             connection.close()
 
+def cleanup_schema(connection = None):
+    connection_passed = connection is not None
+    if not connection_passed:
+        connection = get_connection()
 
+    cursor = connection.cursor()
 
+    sql_statement = """
+    DROP TABLE IF EXISTS tweet CASCADE;
+    DROP TABLE IF EXISTS user_account CASCADE;
+    DROP TABLE IF EXISTS place CASCADE;
+    DROP TABLE IF EXISTS hashtag CASCADE;
+    DROP TABLE IF EXISTS unwound_url CASCADE;
+    DROP TABLE IF EXISTS url CASCADE;
+    DROP TABLE IF EXISTS media CASCADE;
+    DROP TABLE IF EXISTS user_mention CASCADE;
+    """
 
+    try:
+        cursor.execute(sql_statement)
+        connection.commit()
+        print("Schema cleaned up successfully.")
+    except Exception as e:
+        connection.rollback()
+        print(f"An error occurred: {e}")
+    finally:
+        cursor.close()
+        if not connection_passed:
+            connection.close()
