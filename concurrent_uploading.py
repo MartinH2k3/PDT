dabc9322a4b5b202127a74f8f97bbaec6261ffa6
Martin Horský <87446996+MartinH2k3@users.noreply.github.com>
2025-10-04 22:50:32 +0200

    refactor: using environmental variables rather than fixed variables in code

--- concurrent_uploading.py
+++ concurrent_uploading.py
@@ -7,16 +7,15 @@ from schema import *
 from psycopg2.pool import ThreadedConnectionPool
 from logger import Logger
 
-BATCH_SIZE = os.getenv("BATCH_SIZE", 100)
-RETRY_LIMIT = os.getenv("RETRY_LIMIT", 3)
-WORKER_COUNT = os.getenv("WORKER_COUNT", 16)
+BATCH_SIZE = int(os.getenv("BATCH_SIZE", 100))
+RETRY_LIMIT = int(os.getenv("RETRY_LIMIT", 3))
+WORKER_COUNT = int(os.getenv("WORKER_COUNT", 16))
 
 log = Logger()
 
 
 def process_file(tweets_file_path, max_line: int|None = None):
-    conn = pool.getconn()
-    cur = conn.cursor()
+
     line_count = 0
     time_before = time()
     users_batch = []
@@ -57,71 +56,75 @@ def process_file(tweets_file_path, max_line: int|None = None):
         if _tweet.retweeted_status:
             parse_tweet(_tweet.retweeted_status)
 
-    with open(tweets_file_path, 'r') as file:
-        for line in file:
-            if max_line and line_count >= max_line:
-                break
-            tweet_json = json.loads(line)
+    try:
+        conn = pool.getconn()
+        cur = conn.cursor()
+
+        with open(tweets_file_path, 'r') as file:
+            for line in file:
+                if max_line and line_count >= max_line:
+                    break
+                # Skip empty lines
+                if not line.strip():
+                    continue
+                tweet_json = json.loads(line)
+                try:
+                    tweet = Tweet.model_validate(tweet_json)
+                    parse_tweet(tweet)
+
+                except Exception as e:
+                    log.error(f"Error parsing tweet JSON: {e}")
+                    break
+                line_count += 1
+
+                if line_count % BATCH_SIZE == 0:
+                    # try 3 times if deadlock detected, if fails, you will commit these with next batch
+                    for insert_func, args, batch in [
+                        (insert_users, (cur, users_batch), users_batch),
+                        (insert_places, (cur, places_batch), places_batch),
+                        (insert_tweets, (cur, tweets_batch), tweets_batch),
+                        (insert_hashtags_and_link, (cur, tweet.id, hashtags_batch), hashtags_batch),
+                        (insert_urls, (cur, tweet.id, urls_batch), urls_batch),
+                        (insert_medias, (cur, tweet.id, media_batch), media_batch),
+                        (insert_temp_user_mentions, (cur, tweet.id, temp_user_mentions_batch), temp_user_mentions_batch),
+                    ]:
+                        for i in range(RETRY_LIMIT):
+                            try:
+                                insert_func(*args)
+                                conn.commit()
+                                batch.clear()  # Only clear if successful
+                                break
+                            except psycopg2.Error:
+                                log.error(f"Deadlock with {insert_func.__name__}, retrying {i+1}/3", False)
+                                conn.rollback()
+                                sleep(1)
+
+        # insert remaining
+        some_left = True
+        while some_left:
             try:
-                tweet = Tweet.model_validate(tweet_json)
-                parse_tweet(tweet)
-
-            except Exception as e:
-                log.error(f"Error parsing tweet JSON: {e}")
-                break
-            line_count += 1
-
-            if line_count % BATCH_SIZE == 0:
-                # try 3 times if deadlock detected, if fails, you will commit these with next batch
-                for insert_func, args, batch in [
-                    (insert_users, (cur, users_batch), users_batch),
-                    (insert_places, (cur, places_batch), places_batch),
-                    (insert_tweets, (cur, tweets_batch), tweets_batch),
-                    (insert_hashtags_and_link, (cur, tweet.id, hashtags_batch), hashtags_batch),
-                    (insert_urls, (cur, tweet.id, urls_batch), urls_batch),
-                    (insert_medias, (cur, tweet.id, media_batch), media_batch),
-                    (insert_temp_user_mentions, (cur, tweet.id, temp_user_mentions_batch), temp_user_mentions_batch),
-                ]:
-                    for i in range(RETRY_LIMIT):
-                        try:
-                            insert_func(*args)
-                            conn.commit()
-                            batch.clear()  # Only clear if successful
-                            log.info(f"Inserted batch of {len(batch)} using {insert_func.__name__}", False)
-                            break
-                        except psycopg2.Error:
-                            log.error(f"Deadlock with {insert_func.__name__}, retrying {i+1}/3", False)
-                            conn.rollback()
-                            sleep(1)
-
-    # insert remaining
-    some_left = True
-    while some_left:
-        try:
-            insert_users(cur, users_batch)
-            insert_places(cur, places_batch)
-            insert_tweets(cur, tweets_batch)
-            insert_hashtags_and_link(cur, tweet.id, hashtags_batch)
-            insert_urls(cur, tweet.id, urls_batch)
-            insert_medias(cur, tweet.id, media_batch)
-            insert_temp_user_mentions(cur, tweet.id, temp_user_mentions_batch)
-            conn.commit()
-            some_left = False
-        except psycopg2.Error:
-            conn.rollback()
-            sleep(1)
-
-    # return connection to pool
-    pool.putconn(conn)
-
-    # Debug print
-    time_after = time()
-    log.info(f"Inserted {line_count} tweets from {os.path.basename(tweets_file_path)} in {time_after - time_before:.2f} seconds.")
+                insert_users(cur, users_batch)
+                insert_places(cur, places_batch)
+                insert_tweets(cur, tweets_batch)
+                insert_hashtags_and_link(cur, tweet.id, hashtags_batch)
+                insert_urls(cur, tweet.id, urls_batch)
+                insert_medias(cur, tweet.id, media_batch)
+                insert_temp_user_mentions(cur, tweet.id, temp_user_mentions_batch)
+                conn.commit()
+                some_left = False
+            except psycopg2.Error:
+                conn.rollback()
+                sleep(1)
+    finally:
+        # return connection to pool
+        pool.putconn(conn)
+        # Debug print
+        time_after = time()
+        log.info(f"Inserted {line_count} tweets from {os.path.basename(tweets_file_path)} in {time_after - time_before:.2f} seconds.")
 
 
 data_dir = "data"
 jsonl_files = [os.path.join(data_dir, f) for f in os.listdir(data_dir) if f.endswith(".jsonl")]
-
 pool = ThreadedConnectionPool(minconn=1, maxconn=len(jsonl_files), dsn=get_dsn())
 
 seen_ids = set()
@@ -129,10 +132,13 @@ seen_ids_lock = threading.Lock()
 
 total_time_before = time()
 with cf.ThreadPoolExecutor(max_workers=WORKER_COUNT) as executor:
-    futures = [executor.submit(process_file, file_path, 5000) for file_path in jsonl_files]
+    futures = [executor.submit(process_file, file_path) for file_path in jsonl_files]
     # to check if all threads went fine
     for future in cf.as_completed(futures):
-        future.result()
+        try:
+            future.result()
+        except Exception as e:
+            log.error(f"Error in thread: {e}")
 
 total_time_after = time()
 log.info(f"Processed {len(jsonl_files)} files in {total_time_after - total_time_before:.2f} seconds.")
