c845a0f2bdfeb1a4a94d526eddc17f2093c31824
Martin Horský <87446996+MartinH2k3@users.noreply.github.com>
2025-10-05 13:13:09 +0200

    feat: Rebuilding schema also saves entry counts from individual tables

--- rebuild_schema.py
+++ rebuild_schema.py
@@ -1,4 +1,10 @@
-from utils import build_schema, cleanup_schema
+from utils import build_schema, cleanup_schema, count_all_tables
+
+print(count_all_tables())
+with open("history.txt", "a") as f:
+    f.write(f"{count_all_tables()}\n")
 
 cleanup_schema()
+with open("log.txt", "w") as f:
+    f.write("")
 build_schema()
