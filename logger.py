4c6df05b878961f8c89faa9fbc9e63d0dd7f0422
Martin Horský <87446996+MartinH2k3@users.noreply.github.com>
2025-10-06 12:12:41 +0200

    feat: Logger takes log path as argument

--- logger.py
+++ logger.py
@@ -1,15 +1,18 @@
 class Logger:
-    log_path = "log.txt"
-    @staticmethod
-    def info(message: str, to_console: bool = True):
+    def __init__(self, log_path: str = "log.txt"):
+        self.log_path = log_path
+        # Clear the log file on initialization
+        with open(self.log_path, "w") as log_file:
+            log_file.write("")
+
+    def info(self, message: str, to_console: bool = True):
         if to_console:
             print(f"[INFO] {message}")
-        with open(Logger.log_path, "a") as log_file:
+        with open(self.log_path, "a") as log_file:
             log_file.write(f"[INFO] {message}\n")
 
-    @staticmethod
-    def error(message: str, to_console: bool = True):
+    def error(self, message: str, to_console: bool = True):
         if to_console:
             print(f"[ERROR] {message}")
-        with open(Logger.log_path, "a") as log_file:
+        with open(self.log_path, "a") as log_file:
             log_file.write(f"[ERROR] {message}\n")
